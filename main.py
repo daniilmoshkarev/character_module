921e6bac8ee4be9c83c64bdc72a8743039635f67
Даниил Мошкарев <daniil.moshkarev@yandex.ru>
2023-02-14 00:25:04 +0300

    Last version

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+"""Блок функций выбора персонажа, описания персонажа, тренировки,
+а так же зачиты и нападения.
+"""
+
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
-
 def attack(char_name: str, char_class: str) -> str:
+    """Функция подсчёта урона."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +19,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Функция отвечающая за блокировку ударов."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     elif char_class == 'mage':
@@ -24,6 +29,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Функция специальных умений."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -34,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция тренеровки перед боем."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     elif char_class == 'mage':
@@ -58,6 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Функция выбора персонажа."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
