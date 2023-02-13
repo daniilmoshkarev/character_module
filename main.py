555a024492dba6708f549eff0c444930be0ed9dd
Даниил Мошкарев <daniil.moshkarev@yandex.ru>
2023-02-14 00:20:33 +0300

    According to PEP8, docstring

--- main.py
+++ main.py
@@ -79,7 +79,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
